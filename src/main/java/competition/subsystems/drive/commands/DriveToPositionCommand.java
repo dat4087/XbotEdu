3a957b276ffc71d2d4a30ffa2aa868ac66436bf8
Darrell <dat4087@gmail.com>
2023-09-15 18:09:02 -0700

    Update DriveToPositionCommand.java
    completed some exercises

--- src/main/java/competition/subsystems/drive/commands/DriveToPositionCommand.java
+++ src/main/java/competition/subsystems/drive/commands/DriveToPositionCommand.java
@@ -10,6 +10,12 @@ public class DriveToPositionCommand extends BaseCommand {
 
     DriveSubsystem drive;
     PoseSubsystem pose;
+    double currentPosition;
+    double setpoint;
+    double kp, ki, kd;
+    double positionError;
+    static final double error_threshold = 0.2;
+    double driveP;
 
     @Inject
     public DriveToPositionCommand(DriveSubsystem driveSubsystem, PoseSubsystem pose) {
@@ -20,11 +26,17 @@ public class DriveToPositionCommand extends BaseCommand {
     public void setTargetPosition(double position) {
         // This method will be called by the test, and will give you a goal distance.
         // You'll need to remember this target position and use it in your calculations.
+        this.setpoint = position;
     }
 
     @Override
     public void initialize() {
         // If you have some one-time setup, do it here.
+        currentPosition = pose.getPosition();
+        kp = 1000.0;
+        ki = 0.0;
+        kd = 0.0;
+        positionError = setpoint-currentPosition;
     }
 
     @Override
@@ -37,13 +49,21 @@ public class DriveToPositionCommand extends BaseCommand {
 
         // How you do this is up to you. If you get stuck, ask a mentor or student for
         // some hints!
+        currentPosition = pose.getPosition();
+        positionError = setpoint - currentPosition;
+        driveP = positionError*kp;
+        drive.frontLeft.simpleSet(driveP);
+
     }
 
     @Override
     public boolean isFinished() {
         // Modify this to return true once you have met your goal,
         // and you're moving fairly slowly (ideally stopped)
-        return false;
+        if (Math.abs(positionError) <= error_threshold) {
+            drive.frontLeft.simpleSet(0.0);
+            return true;
+        } else return false;
     }
 
 }
